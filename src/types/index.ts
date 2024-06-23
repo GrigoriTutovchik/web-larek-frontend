607af1e3d1445935869f259ee02dbbf59055fa8e
Grigori Turovchik <Jeronimoq49@yandex.ru>
2024-06-23 22:19:58 +0300

    Подготовка документации

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+export interface IProduct {
+  id: string,
+  title: string,
+  description: string,
+  image: string,
+  category: ProductCategory,
+  price: number | null
+} 
+
+export interface IOrder {
+  payment: PaymentMethod,
+  email: string,
+  phone: string,
+  address: string,
+  total: number | null,
+  items: string[]
+}
+
+export interface IBascket {
+  items:string[];
+  total:number;
+}
+
+export interface IOrderResult {
+  id: string;
+  total: number;
+}
+
+export type ProductCategory = 'софт-скил' | 'другое' | 'дополнительное' | 'кнопка' | 'хард-скил';
+
+export type PaymentMethod = 'cash' | 'card';
